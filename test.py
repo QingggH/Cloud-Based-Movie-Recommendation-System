cf90f6bb04bd8549d4e8dfdb45e9b854c01819b7
Yiqing Huang <huangyq200066@163.com>
2025-01-21 17:28:30 -0800

    fix: simplify healthcheck responses and add headers for all responses

--- test.py
+++ test.py
@@ -1,41 +1,27 @@
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/v1/healthcheck', methods=['GET'])
 def healthcheck():
+    # Check if there are query parameters
     if request.args:
-        return make_response(jsonify({'error': 'Bad Request'}), 400)
-    
-    response = make_response(jsonify({'status': 'OK'}), 200)
-    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    return response
-
-@app.route('/v1/healthcheck', methods=['POST', 'PUT', 'DELETE', 'PATCH'])
-def healthcheck_invalid_methods():
-    response = make_response(jsonify({'error': 'Bad Request'}), 400)
-    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    return response
-
-@app.errorhandler(404)
-def not_found_error(error):
-    response = make_response(jsonify({'error': 'Not Found'}), 404)
-    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    return response
+        return '', 400  # Return 400 Bad Request if parameters are present
+    return '', 200  # Return 200 OK if the request is successful
 
 @app.errorhandler(405)
-def method_not_allowed_error(error):
-    response = make_response(jsonify({'error': 'Method Not Allowed'}), 405)
+def method_not_allowed(e):
+    # Return 400 Bad Request for unsupported methods
+    return '', 400
+
+@app.after_request
+def add_headers(response):
+    # Add required headers to all responses
     response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
     response.headers['Pragma'] = 'no-cache'
     response.headers['X-Content-Type-Options'] = 'nosniff'
     return response
 
 if __name__ == '__main__':
+    # Run the server on localhost:8080
     app.run(host='0.0.0.0', port=8080)
